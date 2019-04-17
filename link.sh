527f8f6321016d6ea9b0b0f9d03dde0768527074
hex-344505 <344505@gmail.com>
2019-04-17 12:45:24 +0300

    Added HTTP Requests log to console

--- link.sh
+++ link.sh
@@ -6,6 +6,7 @@ function ctrl_c() {
         echo "Got CTRL-C, lets clean up..."
 	sudo rm /etc/nginx/sites-enabled/$MY_DOMAIN.$HOST_NAME
 	sudo nginx -s reload
+	sudo rm /var/log/nginx/$MY_DOMAIN.$HOST_NAME
 	exit 0
 }
 
@@ -16,11 +17,12 @@ HOST_NAME="`hostname -f`"
 echo "Welcome, let me gather some info..."
 
 S_STRING="`echo $SSH_CLIENT | cut -f1 -d" "` port `echo $SSH_CLIENT | cut -f2 -d" "`"
-CON_PID=`sudo tail -n 300 /var/log/auth.log | grep "$S_STRING" | cut -f2 -d"[" | cut -f1 -d"]" | tail -n 1 `
-CON_PID=`sudo tail -n 300 /var/log/auth.log | grep "$CON_PID" | grep "User child is on pid " | rev | cut -d" " -f1 | rev | tail -n 1`
-MY_DOMAIN=`sudo tail -n 300 /var/log/auth.log | grep "$CON_PID" | grep "tcpip-forward listen" | rev | cut -d" " -f3 | rev | tail -n 1 | sed /_/s//-/`
-MY_PORT=`sudo tail -n 300 /var/log/auth.log | grep "$CON_PID" | grep "tcpip-forward listen" | rev | cut -d" " -f1 | rev | tail -n 1 | cut -f1 -d"."`
-
+CON_PID=`sudo tail -n 300 /var/log/auth.log | grep "$S_STRING" | tail -n 1 | cut -f2 -d"[" | cut -f1 -d"]"`
+CON_PID=`sudo tail -n 300 /var/log/auth.log | grep "$CON_PID" | grep "User child is on pid " | tail -n 1 | rev | cut -d" " -f1 | rev`
+SSTR=`sudo tail -n 300 /var/log/auth.log | grep "$CON_PID" | grep "tcpip-forward listen" | tail -n 1 `
+MY_DOMAIN=`echo $SSTR | rev | cut -d" " -f3 | rev | sed /_/s//-/`
+MY_PORT=`echo $SSTR | rev | cut -d" " -f1 | rev | cut -f1 -d"."`
+MY_PUTTY=`sudo tail -n 300 /var/log/auth.log | grep "$CON_PID" | grep "session_pty_req" | rev | cut -d" " -f1 | rev | tail -n 1 `
 if [ "$MY_PORT" -lt "1024" ]; then
 	echo "Sorry, Mario! But our princess is in another castle!"
 	echo "Try to use port between 1025 and 65535"
@@ -42,6 +44,8 @@ map \$http_upgrade \$connection_upgrade {
 server {
 	listen 80;
 	server_name $MY_DOMAIN.$HOST_NAME;
+	access_log /var/log/nginx/$MY_DOMAIN.$HOST_NAME web-req;
+	error_log /var/log/nginx/$MY_DOMAIN.$HOST_NAME warn;
 	location / {
 	proxy_pass http://127.0.0.1:$MY_PORT;
 	proxy_set_header Host $MY_DOMAIN.$HOST_NAME; 
@@ -58,6 +62,11 @@ EOF
 	echo "All done, your link is https://$MY_DOMAIN.$HOST_NAME"
 
 echo 'Hit CTRL+C to stop tunneling'
+echo 'HTTP Requests'
+echo '-------------'
+
+tail -f /var/log/nginx/$MY_DOMAIN.$HOST_NAME | write link $MY_PUTTY &
+
 while [ "$(ps -q $CON_PID -o cmd=)" != "" ]; do 
 	sleep 5; 
 done
